dae4a517e95092b184220bbf96ba9de553f61775
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2021-09-30 20:36:10 -0700

    price updates

--- lib/pages/tokens.dart
+++ lib/pages/tokens.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:lanthu_bot/components/token_box.dart';
 import 'package:lanthu_bot/models/token.dart';
+import 'package:lanthu_bot/models/token_info.dart';
 import 'package:lanthu_bot/pages/add_token.dart';
 import 'package:http/http.dart' as http;
 import 'package:lanthu_bot/utils/constants.dart';
@@ -44,6 +45,30 @@ class _TokensState extends State<Tokens> {
     return tokens;
   }
 
+  Future<TokenInfo> getTokenInfo(String token) async {
+    var client = http.Client();
+    TokenInfo tokenInfo = const TokenInfo();
+
+    try {
+      var url = Uri.parse('$apiUrl/tokeninfo/${token.toString()}');
+
+      var response = await client.get(url);
+
+      if (response.statusCode == 200) {
+        final resData = json.decode(response.body);
+        if (resData["message"] is! String) {
+          final Map<String, dynamic> message = resData["message"];
+          tokenInfo = TokenInfo.fromMap(message);
+        }
+      }
+    } on SocketException {
+      client.close();
+      throw 'No Internet connection';
+    }
+
+    return tokenInfo;
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -68,6 +93,7 @@ class _TokensState extends State<Tokens> {
                     padding: const EdgeInsets.all(8.0),
                     child: TokenBox(
                       token: tokensList[index],
+                      getTokenInfo: getTokenInfo(tokensList[index].name),
                       onTapEdit: () {
                         Navigator.push(
                           context,
