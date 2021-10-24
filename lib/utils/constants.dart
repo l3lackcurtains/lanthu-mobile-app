4bdac54a2bd8bd825b6af3e4484da4531b6aa628
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2021-10-24 01:11:49 -0700

    url updated

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,3 @@
 const tradeCollection = "trades";
 const tokenCollection = "tokens";
-const apiUrl2 = "https://lanthu.herokuapp.com";
-const apiUrl = "http://192.168.0.7:8000/graphql";
+const apiUrl = "https://lanthu.herokuapp.com/graphql";
